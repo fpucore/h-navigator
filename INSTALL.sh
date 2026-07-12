eb33c69b103ce6285200ce8eed4436329ca610e7
fpucore <chrisjones.unixmen@gmail.com>
2026-07-12 11:56:51 +1000

    Version 4.1.0 released. Tightened H-Linux integration. Added output log display. Added h-swap feature. Code improvements and bug fixes.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
 # -----------------------------------------------------------------------------
-# H Navigator Installer - Version 4.0.0-2
+# H Navigator Installer - Version 4.1.0-1
 # Copyright (c) 2026 Harmonious Platform Systems
 # https://www.freedompublishersunion.net/h-linux.html
 #
-# This Source Code Form is subject to the terms of the Mozilla Public 
-# License, v. 2.0. If a copy of the MPL was not distributed with this 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # -----------------------------------------------------------------------------
 
 # Installation script for H Navigator
 
-# Build and Install the packages
-makepkg -iC || { echo "Build failed. Aborting."; exit 1; }
-# -i = install, C = clean build
+# Build package with -i argument removed. Install handled seperately by NLP
+makepkg -fC || { echo "Build failed. Aborting."; exit 1; }
+#-f - force build
+#-C = clean build
+
+# Install package with NLP
+nlp -U *.zst
 
 # Backup original launch script
 echo "Engaging in pre-patch safety procedure..."
-sudo cp /opt/hlinux/brave-nightly/brave-browser-nightly /opt/hlinux/brave-nightly/brave-browser-nightly.bk
+elevate cp -f /opt/hlinux/brave-nightly/brave-browser-nightly /opt/hlinux/brave-nightly/brave-browser-nightly.bk
 
 # Patch the launch script
 echo "Installing patch..."
-sudo cp patch/brave-browser-nightly /opt/hlinux/brave-nightly/brave-browser-nightly
+elevate cp -f patch/brave-browser-nightly /opt/hlinux/brave-nightly/brave-browser-nightly
 echo "Patched."
 echo
 
 # Clean up installation archives
 echo "Cleaning up my mess..."
 rm *.deb *.zst
+rm -fr src/
+rm -fr pkg/
 echo "Phew... that's done."
 echo
 
@@ -37,11 +43,11 @@ echo "Installing additional system components..."
   # If these two dirs are not created already you are doing something wrong
 mkdir -p $HOME/.hwm
 mkdir -p $HOME/.hwm/scripts
-cp components/h_navigator* $HOME/.hwm/scripts
-cp components/sextant* $HOME/.hwm/scripts
+cp -f components/h_navigator* $HOME/.hwm/scripts
+cp -f components/sextant* $HOME/.hwm/scripts
   # Can be executed by pointing H Core to *exec, but symlinked for compatibility
-ln -s $HOME/.hwm/scripts/h_navigator_exec $HOME/.hwm/scripts/h_navigator
-cp assets/w3c.png $HOME/Pictures/w3c.png
+ln -s -f $HOME/.hwm/scripts/h_navigator_exec $HOME/.hwm/scripts/h_navigator
+cp -f assets/w3c.png $HOME/Pictures/w3c.png
 echo "Additional components installed."
 echo
 
